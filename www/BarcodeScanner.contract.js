2cf42b568f82183db25c22ae402dd9067bad5357
ghadi-weevi <gghraichi@weevi.com>
2026-06-30 12:07:11 +0300

    Add BarcodeScanner contract file

---
+++ www/BarcodeScanner.contract.js
@@ -0,0 +1,7 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var BarcodeScanner_plugin = require('./BarcodeScanner.plugin.js');
+
+exports.MLKitBarcodeScanner = BarcodeScanner_plugin.MLKitBarcodeScanner;
